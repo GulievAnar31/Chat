ebb3e8db73499b510a6ac82827285ebfffb7c76f
GulievAnar <guliev@internet.ru>
2024-07-22 16:54:55 +0300

    fix link

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 const app = () => {
-    const socket = io('http://localhost:3000');
+    const socket = io('https://chat-rfv4.onrender.com');
     const msgInput = document.querySelector('.message-input');
     const msgsList = document.querySelector('.messages-list');
     const sendBtn = document.querySelector('.send-btn');
